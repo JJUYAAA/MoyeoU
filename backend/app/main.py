906fbb84cff490b42ad4669af9faefc0f0e3a196
jjuyaaa <jjjjyy020704@naver.com>
2026-07-16 13:01:31 +0900

    CORS - 모든 도메인 허용

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title=settings.PROJECT_NAME, version="1.0.0")
 # CORS 허용 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://moyeou-eight.vercel.app" ],
+    #allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://moyeou-eight.vercel.app" ],
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
